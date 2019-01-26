f3f02ce7f3817d56615add3d1f0ae7dccaa30dcf
RickyHoo <37119982+RickyHoo@users.noreply.github.com>
2019-01-25 19:34:22 -0500

    Updated validation pt1

--- lab1/src/NameForm.js
+++ lab1/src/NameForm.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 class NameForm extends React.Component {
       constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {value: '' , nameAvailable:false , isValid:true , setValid:props};
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -13,24 +13,34 @@ class NameForm extends React.Component {
       }
 
       handleSubmit(event) {
-        alert('Greetings, '  + this.state.value + '!');
-
+        event.preventDefault();
+      //  alert('Greetings, '  + this.state.value + '!');
+        if(!/[^a-zA-Z] + /.test(this.state.value)) {
+        this.setState({nameAvailable:true})
+        }
+        else{
+          this.state.setValid(false);
+        }
       }
 
 
       render() {
-        return (
-          <div>
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Name:
-              <input type="text" value={this.state.value} onChange={this.handleChange} />
-            </label>
-            <input type="submit" value="Submit" />
-          </form>
-          
-          </div>
-        );
+        let returnArray = []
+        if(!this.state.nameAvailable) {
+          returnArray.push(
+            <form onSubmit={this.handleSubmit} key="main">
+              <label>
+                Name:
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>);
+            return returnArray;
+
+        } else {
+          return (<div>Greetings, {this.state.value}!</div>)
+        }
+
       }
 
 
