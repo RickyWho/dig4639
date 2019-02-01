352050b8812bb8ad2096586d2110792e180a02b6
RickyHoo <37119982+RickyHoo@users.noreply.github.com>
2019-01-31 21:08:46 -0500

    updated validation

--- lab1/src/NameForm.js
+++ lab1/src/NameForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 class NameForm extends React.Component {
       constructor(props) {
         super(props);
-        this.state = {value: '' , nameAvailable:false , isValid:true , setValid:props};
+        this.state = {value: '' , nameAvailable:false , isValid:props.isValid};
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -15,17 +15,18 @@ class NameForm extends React.Component {
       handleSubmit(event) {
         event.preventDefault();
       //  alert('Greetings, '  + this.state.value + '!');
-        if(!/[^a-zA-Z] + /.test(this.state.value)) {
+        if(!/[^a-zA-Z]/.test(this.state.value)) {
         this.setState({nameAvailable:true})
         }
         else{
-          this.state.setValid(false);
+          this.setState({isValid:false})
+          this.setState({nameAvailable:true})
         }
       }
 
-
       render() {
         let returnArray = []
+        const isValid = this.state.isValid
         if(!this.state.nameAvailable) {
           returnArray.push(
             <form onSubmit={this.handleSubmit} key="main">
@@ -36,13 +37,21 @@ class NameForm extends React.Component {
               <input type="submit" value="Submit" />
             </form>);
             return returnArray;
-
-        } else {
-          return (<div>Greetings, {this.state.value}!</div>)
+          }
+          if (isValid) {
+           return (<div>Please enter valid characters.</div>);
+         }
+         return (<div>Greetings, {this.state.value}!</div>);
         }
+
+        //    this.setState({nameAvailable:false})
+        //    this.setState({isValid:false})
+          // {(<div>Greetings, !</div>)}
+      //  }if(this.state.isValid) {
+      //    this.isValid:false
+    //    return
+//(<div>The user is <b>{isValid ? 'currently' : 'not'}</b> logged in.</div>)
 
       }
 
-
-    }
  export default NameForm;
